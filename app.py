de2a68fcdab0d706c5be21f90727ead5442261a1
Gabriel Yashim <gabrileyashim405@gmail.com>
2022-01-23 15:48:16 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 23 15:24:33 2022
+
+@author: YASHIM GABRIEL
+"""
+
+import numpy as np 
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+model = pickle.load(open('GradientBoost.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    
+    output = prediction
+    
+    return render_template('index.html', prediction_text='The crop that will give the best yield is {}'.format(output))
+
+if __name__ == "__main__":
+    app.run(debug=True)
