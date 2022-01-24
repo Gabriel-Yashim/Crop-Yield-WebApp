98bf3644fad0652c4bc615d2517a92222c7d9df2
Gabriel Yashim <gabrileyashim405@gmail.com>
2022-01-24 12:38:23 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ Created on Sun Jan 23 15:24:33 2022
 @author: YASHIM GABRIEL
 """
 
-import numpy as np 
+#import numpy as np 
 from flask import Flask, request, render_template
 import pickle
+from logging import FileHandler,WARNING
 
 app = Flask(__name__)
 model = pickle.load(open('GradientBoost.pkl', 'rb'))
@@ -15,11 +16,13 @@ model = pickle.load(open('GradientBoost.pkl', 'rb'))
 @app.route('/')
 def home():
     return render_template('index.html')
+file_handler = FileHandler('errorlog.txt')
+file_handler.setLevel(WARNING)
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    int_features = [float(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [int_features]
     prediction = model.predict(final_features)
     
     output = prediction
