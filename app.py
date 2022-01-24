b09df6333bfc315889e2f2bcca1314bda4f53520
Gabriel Yashim <gabrileyashim405@gmail.com>
2022-01-24 03:49:55 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ Created on Sun Jan 23 15:24:33 2022
 """
 
 import numpy as np 
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, render_template
 import pickle
 
 app = Flask(__name__)
@@ -18,7 +18,7 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    int_features = [int(x) for x in request.form.values()]
+    int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
     
